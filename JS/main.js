19fd476c19cf0c1c08e7d7374256cc9a912e7d0e
Ahmed_mokhtar <ahmedmohamedmokhtarshalape2002@gmail.com>
2025-05-06 23:39:48 +0300

    Inital commit

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.types', {
+    strings: ["Designer", "Developer"," Larry DanielsDeveloper"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 1000,
+    loop: true
+  });
